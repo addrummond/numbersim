f1fd0f178ccfe95bba4a9a33cc2106e42224fe4d
Alex Drummond <a.d.drummond@gmail.com>
2016-04-14 12:44:44 +0100

    More work on numbersim.c

--- csim/numbersim.c
+++ csim/numbersim.c
@@ -31,11 +31,19 @@ typedef struct state {
     language_t language;
     uint_fast32_t max_cue;
     double assocs[];
+    double learning_rate;
+    double ztnbd_beta;
+    double ztnbd_r;
 } state_t;
 
 static double get_assoc(const state_t *state, uint_fast32_t cue, unsigned marker_index)
 {
     return state->assocs[(cue * num_markers) + marker_index];
+}
+
+static double add_to_assoc(const state_t *state, uint_fast32_t cue, unsigned marker_index, double v)
+{
+    state->assocs[(cue * num_markers) + marker_index] += v;
 }
 
 static void update_state_helper(state_t *state, unsigned marker_index, uint_fast32_t cardinality, double l)
@@ -44,11 +52,25 @@ static void update_state_helper(state_t *state, unsigned marker_index, uint_fast
     for (unsigned i = 0; i < cardinality; ++i) {
         vax += get_assoc(state, i, marker_index);
     }
+
+    double delta_v = state->learning_rate * ((l*1) - vax);
+
+    for (unsigned i = 0; i < trial->cardinality; ++i) {
+        add_to_assoc(state, trial->cardinality, marker_index, delta_v);
+    }
 }
 
 static void update_state(state_t *state, unsigned marker_index, uint_fast32_t cardinality)
 {
+    for (unsigned i = 0; i < state->language.num_markers; ++i) {
+        const char *m = state->language.markers[i];
+        update_state_helper(state, i, cardinality, i == marker_index);
+    }
+}
 
+static void run_trials(state_t *state, unsigned n)
+{
+    
 }
 
 int main()
