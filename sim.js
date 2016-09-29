d6636b81268248118b95c0c603b8f0f82e8d135e
Alex Drummond <a.d.drummond@gmail.com>
2016-09-29 12:58:50 +0100

    Various improvements to sim.js interface.

--- sim.js
+++ sim.js
@@ -2,15 +2,37 @@
 
 let child_process = require('child_process');
 
-const MAX_CARDINALITY = 7;
-const BETA = 0.6;
-const R = 3;
-const LEARNING_RATE = 0.01;
-const N_DISTRIBUTIONS = 10000;
-const N_RUNS = 1000;
-const QUIT_AFTER_N_CORRECT = 200;
+if (process.argv.length != 4 && process.argv.length != 5) {
+    process.stderr.write("Bad arguments\n");
+    process.exit(1);
+}
+let language = process.argv[2];
+let program = process.argv[3];
+let options = {
+    max_cardinality: 7,
+    beta: 0.6,
+    r: 3,
+    learning_rate: 0.01,
+    n_distributions: 10000,
+    n_runs: 10000,
+    quit_after_n_correct: 200
+};
+if (process.argv.length == 5) {
+    try {
+        let j = JSON.parse(process.argv[4]);
+        for (k in j) {
+            if (! ({}).hasOwnProperty.call(j, k))
+                continue;
+            options[k] = j[k];
+        }
+    }
+    catch (e) {
+        process.stderr.write("Bad JSON passed as third argument\n");
+        process.exit(1);
+    }
+}
 
-let rd = new Float64Array(MAX_CARDINALITY-1); // Declared outside of function to avoid allocation on every run.
+let rd = new Float64Array(options.max_cardinality-1); // Declared outside of function to avoid allocation on every run.
 
 function factorial(n) {
     let r = 1;
@@ -30,13 +52,13 @@ function ztnbd(k, beta, r)
 }
 
 function initZtnbDistribution(beta, r, rd) {
-    for (let i = 0; i < MAX_CARDINALITY-1 && i < rd.length; ++i) {
+    for (let i = 0; i < options.max_cardinality-1 && i < rd.length; ++i) {
         rd[i] = ztnbd(i+1, beta, r);
     }
 }
 
 function initRandomDistribution(rd) {
-    let total = Math.random(); // The remaining probability mass for n=max_cardinality.
+    let total = Math.random(); // The remaining probability mass for n=options.max_cardinality.
     for (let i = 0; i < rd.length; ++i) {
         let r = Math.random();
         total += r;
@@ -58,14 +80,14 @@ function initDirichletDistribution(rd)
         return (1 / (g(k) * Math.pow(theta,k))) * Math.pow(x, k - 1) * Math.exp(-(x/theta));
     }
 
-    let tot = gamma(MAX_CARDINALITY, 1, x);
-    for (let i = 0; i < MAX_CARDINALITY-1 && i < rd.length; ++i) {
-        let x = parseInt(Math.round(Math.random()*MAX_CARDINALITY)) + 1;
-        let v = gamma(MAX_CARDINALITY, 1, x);
+    let tot = gamma(options.max_cardinality, 1, x);
+    for (let i = 0; i < options.max_cardinality-1 && i < rd.length; ++i) {
+        let x = parseInt(Math.round(Math.random()*options.max_cardinality)) + 1;
+        let v = gamma(options.max_cardinality, 1, x);
         rd[i] = v;
         tot += v;
     }
-    for (let i = 0; i < MAX_CARDINALITY-1; ++i)
+    for (let i = 0; i < options.max_cardinality-1; ++i)
         rd[i] /= tot;
 }
 
@@ -78,14 +100,14 @@ function getInitialArgs(seed1, seed2) {
     return (
         __dirname + "/languages.txt " +
         seed1 + ' ' + seed2 + ' ' +
-        process.argv[2] + ' ' +
-        BETA + ' ' +
-        R + ' ' +
-        LEARNING_RATE + ' ' +
-        MAX_CARDINALITY + ' ' +
-        N_RUNS + ' ' +
+        language + ' ' +
+        options.beta + ' ' +
+        options.r + ' ' +
+        options.learning_rate + ' ' +
+        options.max_cardinality + ' ' +
+        options.n_runs + ' ' +
         'summary ' +
-        QUIT_AFTER_N_CORRECT + ' '
+        options.quit_after_n_correct + ' '
     );
 }
 
@@ -115,7 +137,7 @@ numbersim.stderr.pipe(process.stderr);
 
 let programs = { };
 programs.default = function () {
-    this.numberOfFails = new Uint32Array(MAX_CARDINALITY); // Will be initialized with zeros
+    this.numberOfFails = new Uint32Array(options.max_cardinality); // Will be initialized with zeros
 
     this.setupDistribution = () => {
         //initRandomDistribution(rd);
@@ -123,14 +145,13 @@ programs.default = function () {
     };
 
     this.handleLine = (cols) => {
-        if (cols.length != MAX_CARDINALITY + 2)
+        if (cols.length != options.max_cardinality + 2)
             return;
 
-        for (let i = 0; i < MAX_CARDINALITY; ++i)
+        for (let i = 0; i < options.max_cardinality; ++i)
             this.numberOfFails[i] += (parseInt(cols[i]) == -1 ? 1 : 0);
 
-        ++numRuns;
-        if (numRuns < N_DISTRIBUTIONS) {
+        if (numRuns < options.n_distributions) {
             doRun(seed1, seed2);
         }
         else {
@@ -140,16 +161,16 @@ programs.default = function () {
     };
 
     this.printFinalReport = () => {
-        for (let i = 0; i < MAX_CARDINALITY; ++i) {
-            console.log(i+1, ((1-(this.numberOfFails[i]/N_DISTRIBUTIONS))*100) + '%');
+        for (let i = 0; i < options.max_cardinality; ++i) {
+            console.log(i+1, ((1-(this.numberOfFails[i]/options.n_distributions))*100) + '%');
         }
     };
 };
 
 programs.compare = function () {
     this.state = 'random';
-    this.ztnbd = new Float64Array(MAX_CARDINALITY-1);
-    this.random = new Float64Array(MAX_CARDINALITY-1);
+    this.ztnbd = new Float64Array(options.max_cardinality-1);
+    this.random = new Float64Array(options.max_cardinality-1);
     initZtnbDistribution(0.6, 3, this.ztnbd);
 
     this.setupDistribution = () => {
@@ -165,7 +186,6 @@ programs.compare = function () {
     };
 
     this.handleLine = (cols) => {
-
         if (this.state == 'random') {
             // Compute distance from ztnbd.
             let tot = 0;
@@ -177,25 +197,29 @@ programs.compare = function () {
 
             // Compute average success rate.
             let succ = 0;
-            for (let i = 0; i < MAX_CARDINALITY; ++i) {
+            for (let i = 0; i < options.max_cardinality; ++i) {
                 let v = parseFloat(cols[i]);
                 if (v != -1)
                     succ += v;
             }
-            succ /= MAX_CARDINALITY;
+            succ /= options.max_cardinality;
 
             console.log(tot + ',' + succ);
         }
     };
 };
 
-let program = new programs.compare;
+let progf = new programs[program];
+if (progf === undefined) {
+    process.stderr.write("Bad program name");
+    process.exit(1);
+}
 
 let currentBuffer = "";
 let currentBufferIndex = 0;
 let numRuns = 0;
 let seed1, seed2;
-let numberOfFails = new Uint32Array(MAX_CARDINALITY); // Will be initialized with zeros
+let numberOfFails = new Uint32Array(options.max_cardinality); // Will be initialized with zeros
 numbersim.stdout.on('data', (data) => {
     currentBuffer += data;
 
@@ -208,14 +232,14 @@ numbersim.stdout.on('data', (data) => {
             currentBufferIndex = 0;
             seed1 = parseInt(cols[cols.length-2]);
             seed2 = parseInt(cols[cols.length-1]);
-            program.handleLine(cols);
+            progf.handleLine(cols);
             ++numRuns;
-            if (numRuns < N_DISTRIBUTIONS) {
+            if (numRuns < options.n_distributions) {
                 doRun(seed1, seed2);
             }
             else {
-                if (program.printFinalReport)
-                     program.printFinalReport();
+                if (progf.printFinalReport)
+                     progf.printFinalReport();
                 process.exit(0);
             }
             break;
@@ -225,7 +249,7 @@ numbersim.stdout.on('data', (data) => {
 
 doRun();
 function doRun(seed1, seed2) {
-    program.setupDistribution();
+    progf.setupDistribution();
     let cmd = getInitialArgs(seed1, seed2) + rd.join(' ') + '\n';
     //console.log("-->", cmd);
     numbersim.stdin.write(cmd, 'utf-8');
